26c003e99d3cd1d34287f90230fc3da9608c428f
nehal1201 <thakkarnehal44@gmail.com>
2025-05-01 12:26:21 -0400

    added Script.js

---
+++ ReVive-app/script.js
@@ -0,0 +1,34 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    })
+    .then(res => {
+      if (res.status === 200) return res.text();
+      else throw new Error("Login failed");
+    })
+    .then(msg => alert(msg))
+    .catch(err => alert(err.message));
+  });
+  
+  document.getElementById("signupBtn").addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    fetch("/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    })
+    .then(res => {
+      if (res.status === 200) return res.text();
+      else throw new Error("Signup failed or user already exists");
+    })
+    .then(msg => alert(msg))
+    .catch(err => alert(err.message));
+  });
+  
